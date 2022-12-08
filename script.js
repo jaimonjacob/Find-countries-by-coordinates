60244a11afe939d601a92e8dd600071e36a8ba0e
jaimonjacob <jaimon.jacob2009@gmail.com>
2022-12-08 11:14:07 +0530

    2 files modified

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const artContainer = document.querySelector('.cardsContainer');
 const form = document.querySelector(`.search--form`);
+const searchInstr = document.querySelector('.search--instr');
+const appInfo = document.querySelector('.app--info');
+searchInstr.textContent = `Enter the coordinates separated by comma to see the country`
+appInfo.textContent = `This site is an API testing app created by Jaimon. Please use the form below to see the latest data. This app lets you enter coordinates separated by comma, for example - 23,78 - and the app will retrieve the country that the coordinates belong to.`
+
 const formInput = document.querySelector(`.search--text`);
 
 const renderData = function (inputData) {
@@ -40,36 +45,41 @@ const renderError = function (error) {
   );
 };
 
-const getCountryData = function (e) {
-  e.preventDefault();
-  artContainer.replaceChildren();
-  const [lat, lng] = formInput.value.split(`,`);
-  fetch(
-    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
-  )
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(
-          `The entered coordinates do not represent any country - The error code is ${response.status}`
-        );
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-      return fetch(`https://restcountries.com/v3.1/alpha/${data.countryCode}`);
-    })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(
-          `There is no such country code - The error code is ${response.status}`
-        );
-      }
-      return response.json();
-    })
-    .then((data) => renderData(data))
-    .catch((err) => renderError(err))
-    .finally((message) => console.log(`The request is processed`));
-};
+
+//USING ASYNC
+//Use the async keyword to convert any function to async
+const getCountryData = async function (e) {
+  //Using try--catch to handle errors
+  try {
+
+    e.preventDefault();
+    artContainer.replaceChildren();
+    const [lat, lng] = formInput.value.split(`,`);
+    //Use await keyword to make any command wait
+    const response = await fetch(
+      `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`);
+    const data = await response.json()
+    //Creating a custom error to  handle error of invalid entry
+    if (!response.ok) {
+      throw new Error(
+        `The entered coordinates are invalid; please recheck - The error code is ${response.status}`
+      )
+    }
+
+    const countryresponse = await fetch(`https://restcountries.com/v3.1/alpha/${data.countryCode}`);
+    const countryData = await countryresponse.json();
+    //Creating a custom error to  handle error of invalid input
+    if (!countryresponse.ok) {
+      throw new Error(
+        `The coordinates do not belong to a country; mostly belong to sea or the poles :-) - The error code is ${response.status}`
+      )
+    }
+
+    renderData(countryData)
+  } catch (err) {//Catching and acting on error
+    renderError(err)
+  }
+
+}
 
 form.addEventListener(`submit`, getCountryData);
